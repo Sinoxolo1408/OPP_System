14acffd7be21278c0b619f6dcaac82f9d9667b48
Sinoxolo Mpetsheni <56871809+Sinoxolo1408@users.noreply.github.com>
2020-06-30 17:04:38 +0200

    Add files via upload

---
+++ OPPGui/src/oppgui/OPPGui.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oppgui;
+
+/**
+ *
+ * @author Sinoxolo S Mpetsheni
+ */
+public class OPPGui {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
